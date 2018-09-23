62981cf89f2918d8ff05416dc8361d56ac6192cf
Imar Rubio <irubio@skolpad.org>
2018-09-23 15:27:28 +0800

    Improve users screen labels table

--- src/components/Dashboard/Views/Tables/PaginatedTables.vue
+++ src/components/Dashboard/Views/Tables/PaginatedTables.vue
@@ -42,7 +42,6 @@
               fixed="right"
               label="Actions">
               <template slot-scope="props">
-                <a class="btn btn-simple btn-xs btn-info btn-icon like" @click="handleLike(props.$index, props.row)"><i class="ti-heart"></i></a>
                 <a class="btn btn-simple btn-xs btn-warning btn-icon edit" @click="handleEdit(props.$index, props.row)"><i class="ti-pencil-alt"></i></a>
                 <a class="btn btn-simple btn-xs btn-danger btn-icon remove"  @click="handleDelete(props.$index, props.row)"><i class="ti-close"></i></a>
               </template>
@@ -139,17 +138,12 @@
           },
           {
             prop: 'email',
-            label: 'Email',
+            label: 'Department',
             minWidth: 250
           },
           {
             prop: 'age',
-            label: 'Age',
-            minWidth: 100
-          },
-          {
-            prop: 'salary',
-            label: 'Salary',
+            label: 'Status',
             minWidth: 120
           }
         ],
